2053c414e8cfbe959be6f44e82201123c8db936e
Diksha2256 <dikshajadhav2256@gmail.com>
2026-07-19 09:22:39 +0000

    add conversionoperation.js

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,22 @@
+let a="33abc";
+let c=Number(a);
+console.log(c);
+console.log(typeof c);
+//"33" =>33
+//"12abc" =>NaN
+//true =>1 ; false=>0
+
+let n=1;
+let booleann=Boolean(n);
+console.log(booleann);
+console.log(typeof booleann);
+
+//1 =>true
+//0 =>false
+//"" =>false
+//"diksha" => true
+
+let num=14;
+let str=String(num);
+console.log(str);
+console.log(typeof str);
