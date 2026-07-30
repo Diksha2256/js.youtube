1314a6fa7e949dc1c40812a59b6fbd438da6f738
Diksha2256 <dikshajadhav2256@gmail.com>
2026-07-30 22:10:24 +0530

    Delete 04IIFE.js

--- 04IIFE.js
+++
@@ -1,9 +0,0 @@
-//Immediately Invoked function Expression
-//they stop global scope pollution
-(function chai(){
-    console.log("DB Connected");
-})();     //named IIFE
-
-( (name) => {
-    console.log(`DB connected Two ${name}`);
-})("diksha")     //unnamed and paraet IIFE
